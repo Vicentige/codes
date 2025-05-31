08dfa221ad372dd4f5befceb69e2a1d9208639e5
Vicentige <vicentige@gmail.com>
2025-05-31 12:29:49 -0400

    version estable xd

--- Tareas/Taller3/Taller3.c
+++ Tareas/Taller3/Taller3.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
-#define MAX 100
+#define MAX 8
+int separar_rut(int, int []);
+int multi(int[], int );
 
-int main(){
+int main()  {  
+    int rut, suma, rut_digitos[MAX];
+    char digito_verificador;
     
+    // Ingresar rut
+    printf("Ingrese el rut sin digito verificador: ");
+    scanf("%d", &rut);
+
+    printf("Ingrese el digito verificador: ");
+    scanf(" %c", &digito_verificador);
+
+    separar_rut(rut, rut_digitos);
+    suma = multi(rut_digitos, MAX);
+    printf("Suma: %d", suma);
+    return 0;
+} 
+
+int separar_rut(int rut, int rut_digitos[]) {
+    for (int i = 0; i < MAX; i++) {
+        rut_digitos[i] = rut % 10;
+        rut /= 10;
+    }
+}
+
+int multi(int rut_digitos[], int n) {
+    int suma = 0, multiplicador = 2;
+    for (int i = 0; i < n; i++) {
+        suma += rut_digitos[i] * multiplicador;
+        multiplicador++;
+        if (multiplicador > 7) multiplicador = 2;
+    }
+    return suma;
 }
