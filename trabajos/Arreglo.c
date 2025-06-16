4c824041cb3e3097472c67086816224c13689d23
Vicentige <vicentige@gmail.com>
2025-06-15 20:34:40 -0400

    asda

---
+++ trabajos/Arreglo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAX 100
+
+int main(){
+    int numeros[MAX], num, i = 0;
+    FILE *archivo;
+    archivo = fopen("numeros.txt", "r");
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo.\n");    
+    }
+    while(i < 10){
+        if (fscanf(archivo, "%d",))
+
+    }
+
+}
